e30d9fbb0e81c732f59761cb14a36910dd30fafa
cxnzensei <nishitpatil3231@gmail.com>
2021-11-23 13:50:24 +0530

    removed  firebase config from gitignore

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,8 +2,17 @@ import Firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 import { getStorage } from "firebase/storage";
 import "firebase/compat/auth";
-import config from "../config";
 import { getFirestore } from "firebase/firestore";
+
+const config = {
+	apiKey: "AIzaSyBjQynIJfbg2cnlWqJzSfalwbSJsSke6n8",
+	authDomain: "finstagram-82ec3.firebaseapp.com",
+	projectId: "finstagram-82ec3",
+	storageBucket: "finstagram-82ec3.appspot.com",
+	messagingSenderId: "813946363613",
+	appId: "1:813946363613:web:b219099cbc478d7e820534",
+	measurementId: "G-8RT62BPCKV",
+};
 
 const firebase = Firebase.initializeApp(config);
 const { FieldValue } = Firebase.firestore;
